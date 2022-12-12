64fc1179a3861b438b3f94a33f32755ae0eaf8da
Yoko Kawamoto <yokokawamoto@YokonoMBP.hsd1.wa.comcast.net>
2022-12-11 19:48:05 -0800

    Nav bar set for large/small screen. Icon package added

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
-
+import {Bars3Icon} from '@heroicons/react/24/solid'
 
 export default function Header():React.ReactElement{
   return(
   <div className="flex p-4 w-full bg-stone-800 text-white bounce-once justify-between md:justify-start">
     <div>Logo</div>
-    <nav className="mid: pl-12">Menu</nav>
+    <nav className="mid: pl-12">
+      <Bars3Icon className="h-6 w-6 md:hidden"/>
+      <ul className='hidden md:flex gap-8 list-none'>
+        <li>
+          About
+        </li>
+        <li>
+          Skills
+        </li>
+        <li>
+          Projects
+        </li>
+      </ul>
+    </nav>
   </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,9 @@ export default function Home() {
         <title>Portfolio</title>
         <meta name="description" content="Yoko's portfolio" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&family=Raleway:wght@400;500&display=swap" rel="stylesheet" />
       </Head>
       <div className="min-h-screen flex flex-col p-0">
         <Header/>
