20af5ebbeae6cd2e6e732f5ac735c9320eeaf2b7
Yoko Kawamoto <yokokawamoto@YokonoMacBook-Pro.local>
2022-12-21 18:33:03 -0800

    Adjusted card margin

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -14,23 +14,28 @@ export default function Card({
   url
 }: Props): ReactElement {
   return (
-    <div className="rounded-md flex-1 p-3 border border-stone-500">
-      <h1 className="font-bold">{title}</h1>
-      <div className="font-light text-stone-400">{description}</div>
-      <div className="flex gap-2 py-2">
-        {badges.map((item, index) => (
-          <div key={index} className="px-2 border border-stone-500 rounded-sm">
-            {item}
-          </div>
-        ))}
-      </div>
-      {url != null && (
-        <div className="px-2 w-14 bg-stone-500 text-white text-center rounded-sm  hover:animate-pulse">
-          <Link href={url} target="_blank">
-            Visit
-          </Link>
+    <div className="p-2">
+      <div className="flex flex-col p-2 flex-1 rounded-md border border-stone-500">
+        <h1 className="font-bold">{title}</h1>
+        <div className="font-light text-stone-400">{description}</div>
+        <div className="flex gap-2 py-2">
+          {badges.map((item, index) => (
+            <div
+              key={index}
+              className="px-2 border border-stone-500 rounded-sm"
+            >
+              {item}
+            </div>
+          ))}
         </div>
-      )}
+        {url != null && (
+          <div className="px-2 w-14 bg-stone-500 text-white text-center rounded-sm  hover:animate-pulse place-self-end">
+            <Link href={url} target="_blank">
+              Visit
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
