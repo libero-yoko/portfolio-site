661caf32f176b8770c652950ed07a44330a850b9
Yoko Kawamoto <yokokawamoto@YokonoMacBook-Pro.local>
2022-12-23 19:52:47 -0800

    Floodfill implementation

--- components/algo/floodfill.tsx
+++ components/algo/floodfill.tsx
@@ -1,4 +1,6 @@
-export const get2DArray = (x: number, y: number): Array<Array<number>> => {
+import { useEffect, useState } from 'react'
+
+const get2DArray = (x: number, y: number): Array<Array<number>> => {
   const col = []
   for (let i = 0; i < y; i++) {
     const row = []
@@ -10,4 +12,127 @@ export const get2DArray = (x: number, y: number): Array<Array<number>> => {
   return col
 }
 
-export const floodFill = () => {}
+export default function FloodFill() {
+  const [x, setX] = useState<string>('')
+  const [y, setY] = useState<string>('')
+  const [array, setArray] = useState<Array<Array<number>>>([[]])
+  useEffect(() => {
+    updateTile(5, 5)
+  }, [])
+
+  useEffect(() => {
+    console.log(x, y)
+  }, [x, y])
+
+  function validateNum(num: number): number {
+    if (num < 0) {
+      num = 0
+    }
+    if (num > 10) {
+      num = 10
+    }
+    return num
+  }
+
+  const updateTile = (x: number, y: number) => {
+    const newY = validateNum(y)
+    setY(newY.toString())
+    const newX = validateNum(x)
+    setX(newX.toString())
+    const array = get2DArray(newX, newY)
+    setArray(array)
+  }
+
+  const floodFill = (
+    y: number,
+    x: number,
+    newColor: number,
+    targetColor: number
+  ): void => {
+    if (
+      x >= array[0].length ||
+      y >= array.length ||
+      y < 0 ||
+      x < 0 ||
+      array[y][x] === newColor
+    ) {
+      return
+    }
+    if (array[y][x] === targetColor && array[y][x] != newColor) {
+      const newArray = [...array]
+      newArray[y][x] = newColor
+      setArray(newArray)
+      setTimeout(() => floodFill(y + 1, x, newColor, targetColor), 300)
+      setTimeout(() => floodFill(y - 1, x, newColor, targetColor), 300)
+      setTimeout(() => floodFill(y, x + 1, newColor, targetColor), 300)
+      setTimeout(() => floodFill(y, x - 1, newColor, targetColor), 300)
+    }
+  }
+  return (
+    <>
+      {array?.map((row, index) => {
+        let rowIndex = index
+        return (
+          <div className="flex justify-center" key={index}>
+            <div className="flex">
+              {row.map((num, index) => {
+                let colIndex = index
+                let cellColor
+                num === 0
+                  ? (cellColor = 'bg-[#F65A5A]') // red
+                  : num === 1
+                  ? (cellColor = 'bg-[#75B1DF]') // blue
+                  : (cellColor = 'bg-[#F3EED9]') // white
+                return (
+                  <div
+                    key={index}
+                    onClick={() => {
+                      let currentColor = array[rowIndex][colIndex]
+                      floodFill(rowIndex, colIndex, 2, currentColor)
+                    }}
+                    className={`w-8 h-8 text-center flex justify-center items-center ${cellColor} border border-stone-400 rounded-full`}
+                  ></div>
+                )
+              })}
+            </div>
+          </div>
+        )
+      })}
+      <div className="flex justify-center m-4 gap-8">
+        <label className="flex self-center">
+          <div className="text-stone-600 mr-2">X</div>
+          <input
+            min="0"
+            max="10"
+            type="number"
+            id="x"
+            title="X"
+            value={x ?? 0}
+            className="flex w-sm text-stone-500 border boder-stone-800 rounded-sm"
+            onChange={(e) => {
+              updateTile(Number(e.target.value), parseInt(y))
+            }}
+          ></input>
+        </label>
+        <label className="flex self-center">
+          <div className="text-stone-600 mr-2">Y</div>
+          <input
+            min="0"
+            max="15"
+            type="number"
+            id="y"
+            title="Y"
+            value={y ?? 0}
+            className="flex w-sm text-stone-500 border boder-stone-800 rounded-sm"
+            onChange={(e) => {
+              updateTile(parseInt(x), Number(e.target.value))
+            }}
+          ></input>
+        </label>
+      </div>
+      <button onClick={() => updateTile(parseInt(x), parseInt(y))}>
+        Reset
+      </button>
+    </>
+  )
+}
--- pages/Algo.tsx
+++ pages/Algo.tsx
@@ -1,99 +1,12 @@
 import { ReactElement, useEffect, useState } from 'react'
-import { get2DArray } from '../components/algo/floodfill'
+import FloodFill from '../components/algo/floodfill'
+
 import Layout from '../components/layout/Layout'
 
 export default function Algo(): ReactElement {
-  const [x, setX] = useState<string>('')
-  const [y, setY] = useState<string>('')
-  const [array, setArray] = useState<Array<Array<number>> | null>(null)
-  useEffect(() => {
-    updateTile(5, 5)
-  }, [])
-
-  useEffect(() => {
-    console.log(x, y)
-  }, [x, y])
-
-  function validateNum(num: number): number {
-    if (num < 0) {
-      num = 0
-    }
-    if (num > 10) {
-      num = 10
-    }
-    return num
-  }
-
-  const updateTile = (x: number, y: number) => {
-    const newY = validateNum(y)
-    setY(newY.toString())
-    const newX = validateNum(x)
-    setX(newX.toString())
-
-    const array = get2DArray(newX, newY)
-    setArray(array)
-  }
   return (
     <Layout title="Algo">
-      <>
-        {array?.map((row, index) => {
-          return (
-            <div className="flex justify-center" key={index}>
-              <div className="flex">
-                {row.map((num, index) => {
-                  let cellColor
-                  num === 0
-                    ? (cellColor = 'bg-yellow')
-                    : num === 1
-                    ? (cellColor = 'bg-pink')
-                    : (cellColor = 'bg-green')
-                  return (
-                    <div
-                      key={index}
-                      className={`w-8 h-8 text-center flex justify-center items-center ${cellColor} border border-white`}
-                    ></div>
-                  )
-                })}
-              </div>
-            </div>
-          )
-        })}
-        <div className="flex flex-col justify-center m-4">
-          <label className="flex self-center">
-            <div className="text-stone-600">X</div>
-            <input
-              min="0"
-              max="10"
-              type="number"
-              id="x"
-              title="X"
-              value={x ?? 0}
-              className="flex w-sm text-stone-500 border boder-stone-800 rounded-sm"
-              onChange={(e) => {
-                updateTile(Number(e.target.value), parseInt(y))
-              }}
-            ></input>
-          </label>
-          <label className="flex self-center">
-            <div className="text-stone-600">Y</div>
-            <input
-              min="0"
-              max="15"
-              type="number"
-              id="y"
-              title="Y"
-              value={y ?? 0}
-              className="flex w-sm text-stone-500 border boder-stone-800 rounded-sm"
-              onChange={(e) => {
-                updateTile(parseInt(x), Number(e.target.value))
-              }}
-            ></input>
-          </label>
-          <button onClick={() => updateTile(parseInt(x), parseInt(y))}>
-            Reset
-          </button>
-        </div>
-      </>
+      <FloodFill />
     </Layout>
   )
 }
